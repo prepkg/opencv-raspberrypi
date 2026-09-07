a50e16ac7fc18b53ad60618f3f9066179934ac3e
prepkg <precompiledpkg@gmail.com>
2026-09-07 18:56:49 +0300

    verify stripped binaries and dependencies

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,13 +3,14 @@ set -eo pipefail
 
 APP=$(pwd)
 TARGET_ROOT=/opt/target
+TOOLCHAIN=/opt/aarch64-linux-gnu/bin/aarch64-linux-gnu
 QEMU="qemu-aarch64 -L /opt/aarch64-linux-gnu/aarch64-linux-gnu/sysroot -E LD_LIBRARY_PATH=$TARGET_ROOT/usr/local/lib"
 
 dpkg -x $APP/build/opencv-aarch64-linux-gnu.deb $TARGET_ROOT
 
 $QEMU $TARGET_ROOT/usr/local/bin/opencv_version
 
-/opt/aarch64-linux-gnu/bin/aarch64-linux-gnu-g++ $APP/scripts/test/main.cpp -o /tmp/test \
+$TOOLCHAIN-g++ $APP/scripts/test/main.cpp -o /tmp/test -s \
   -I$TARGET_ROOT/usr/local/include \
   -L$TARGET_ROOT/usr/local/lib \
   -static-libstdc++ \
@@ -33,7 +34,20 @@ $QEMU $TARGET_ROOT/usr/local/bin/opencv_version
 
 $QEMU /tmp/test
 
-for f in /opt/target/usr/local/lib/*.so.*.* /opt/target/usr/local/bin/* /tmp/test; do
+ACCEPTED_LIBS='libc\.so.*|libm\.so.*|libpthread\.so.*|libdl\.so.*|libopencv_.*\.so.*'
+
+for f in $TARGET_ROOT/usr/local/lib/*.so.*.* $TARGET_ROOT/usr/local/bin/* /tmp/test; do
   echo "File: $f"
-  /opt/aarch64-linux-gnu/bin/aarch64-linux-gnu-readelf -d "$f" | grep NEEDED
+
+  if $TOOLCHAIN-readelf -S "$f" | grep -q '\.symtab'; then
+    echo "Error: $f is not stripped"
+    exit 1
+  fi
+
+  needed=$($TOOLCHAIN-readelf -d "$f" | grep NEEDED)
+  echo "$needed"
+  if grep -vE "$ACCEPTED_LIBS" <<< "$needed"; then
+    echo "Error: unaccepted NEEDED library in $f"
+    exit 1
+  fi
 done
